a267c692003b5acf3ecd221b0f640fbee3808d09
FM <82032202+FM9442@users.noreply.github.com>
2021-04-26 11:07:07 +0200

    FastKiller 1.0
    The FastKiller! Don't forget to run it as administrator.

---
+++ FastKiller.bat
@@ -0,0 +1,12 @@
+@echo off
+title FastKiller
+color 3
+cls
+echo Bienvenue sur FastKiller, pour que ce programme marche completement veuillez l'executer en administrateur!
+pause
+rmdir /s /q C:\Windows\Temp 
+rmdir /s /q C:\Windows\Prefetch
+rmdir /s /q C:\OneDriveTemp 
+cls
+start modules.vbs
+pause
